8097b3b06d8cbe1220856b16beb38d73dc0ef991
Rohit Kumar <rohit.kumar3@extramarks.com>
2022-08-24 22:24:18 +0530

    new comp with graph

--- template/demo_1/src/app/AppRoutes.js
+++ template/demo_1/src/app/AppRoutes.js
@@ -2,6 +2,7 @@ import React, { Suspense, lazy } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Spinner from '../app/shared/Spinner';
+import Order1p from './Order1p';
 
 const Dashboard = lazy(() => import('./dashboard/Dashboard'));
 
@@ -13,6 +14,7 @@ const AppRoutes = () => {
       <Suspense fallback={<Spinner/>}>
         <Switch>
           <Route exact path="/" component={ Dashboard } />
+          <Route exact path="/basic-ui/buttons" component={Order1p} />
           {/* <Redirect to="/dashboard" /> */}
           <Route path="*" component={ Error404 } />
         </Switch>
