c71103a51cd8f34ed5ff1465f3925acfc1fa8060
Rohit Kumar <rohit.kumar3@extramarks.com>
2022-08-25 16:30:10 +0530

    added jest

--- template/demo_1/src/app/App.test.js
+++ template/demo_1/src/app/App.test.js
@@ -1,9 +1,11 @@
+import { BrowserRouter } from 'react-router-dom';
 import React from '../../node_modules/react';
 import ReactDOM from '../../node_modules/react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  it('Should do sum', () => {
+    let sum = 5
+    expect(sum).toBe(5)
+  })
+})
