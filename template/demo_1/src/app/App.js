2f4f5ec1474a4f95d627357750db2fa5d29e407e
Rohit Kumar <rohit.kumar3@extramarks.com>
2022-08-25 17:11:11 +0530

    removed some xtra code

--- template/demo_1/src/app/App.js
+++ template/demo_1/src/app/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 import { useLocation, withRouter } from "react-router-dom";
 import "./App.scss";
@@ -7,62 +7,16 @@ import AppRoutes from "./AppRoutes";
 import Navbar from "./shared/Navbar";
 import Sidebar from "./shared/Sidebar";
 
-import { useTranslation, withTranslation } from "react-i18next";
-
 const App = () => {
-  const [state, setState] = useState({});
-  const { i18n } = useTranslation();
   const location = useLocation();
   useEffect(() => {
-    onRouteChanged();
+    window.scrollTo(0, 0);
   }, [location]);
 
-  const onRouteChanged = () => {
-    const body = document.querySelector("body");
-    if (location.pathname === "/layout/RtlLayout") {
-      body.classList.add("rtl");
-      i18n.changeLanguage("ar");
-    } else {
-      body.classList.remove("rtl");
-      i18n.changeLanguage("en");
-    }
-    window.scrollTo(0, 0);
-    const fullPageLayoutRoutes = [
-      "/user-pages/login-1",
-      "/user-pages/register-1",
-      "/user-pages/lockscreen",
-      "/error-pages/error-404",
-      "/error-pages/error-500",
-      "/general-pages/landing-page",
-    ];
-    for (let i = 0; i < fullPageLayoutRoutes.length; i++) {
-      if (location.pathname === fullPageLayoutRoutes[i]) {
-        setState({
-          isFullPageLayout: true,
-        });
-        document
-          .querySelector(".page-body-wrapper")
-          .classList.add("full-page-wrapper");
-        break;
-      } else {
-        setState({
-          isFullPageLayout: false,
-        });
-        document
-          .querySelector(".page-body-wrapper")
-          .classList.remove("full-page-wrapper");
-      }
-    }
-  };
-
-  let navbarComponent = !state.isFullPageLayout ? <Navbar /> : "";
-  let sidebarComponent = !state.isFullPageLayout ? <Sidebar /> : "";
-
   return (
-    <div className="container-scroller">
-      {navbarComponent}
+    <div className="container-scroller"><Navbar />
       <div className="container-fluid page-body-wrapper">
-        {sidebarComponent}
+        <Sidebar />
         <div className="main-panel">
           <div className="content-wrapper">
             <AppRoutes />
@@ -73,4 +27,4 @@ const App = () => {
   );
 };
 
-export default withTranslation()(withRouter(App));
+export default (withRouter(App));
