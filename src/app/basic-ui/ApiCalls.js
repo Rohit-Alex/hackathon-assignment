cac7180b25d386f57c939d98e8847f9abdbc3107
Rohit Kumar <rohit.kumar3@extramarks.com>
2022-08-26 10:40:49 +0530

    retrieved removed code

--- src/app/basic-ui/ApiCalls.js
+++ src/app/basic-ui/ApiCalls.js
@@ -1,47 +1,76 @@
 import { makeHttpRequest } from "../../utils";
 
-export const getTodosList = () => {
+export const getOrderFailedCount = () => {
     return new Promise((resolve, reject) => {
         makeHttpRequest({
-            path: 'https://dd17-117-217-192-41.in.ngrok.io/v1/orderInfo/count',
+            path: "http://localhost:8080/v1/orderInfo/count",
             method: "get",
         })
             .then((res) => {
-                /* 
-                {
-                    "data": {
-                        "eventType": "All",
-                        "count": 1
-                    }
-                }
-                */
                 resolve(res || {});
             })
             .catch((err) => {
                 reject(err || {});
             });
     });
-}
+};
 
 export const getEventList = () => {
     return new Promise((resolve, reject) => {
         makeHttpRequest({
-            path: 'https://dd17-117-217-192-41.in.ngrok.io/v1/orderInfo/count',
+            path: "http://localhost:8080/v1/workflow/stages-with-count/OrderFlow",
             method: "get",
         })
             .then((res) => {
-                /* 
-                {
-                    "data": {
-                        "eventType": "All",
-                        "count": 1
-                    }
-                }
-                */
                 resolve(res || {});
             })
             .catch((err) => {
                 reject(err || {});
             });
     });
-}
+};
+
+export const getDashboardData = () => {
+    return new Promise((resolve, reject) => {
+        makeHttpRequest({
+            path: "http://localhost:8080/v1/orderInfo/count",
+            method: "get",
+        })
+            .then((res) => {
+                resolve(res || {});
+            })
+            .catch((err) => {
+                reject(err || {});
+            });
+    });
+};
+
+export const getTableData = (eventId) => {
+    return new Promise((resolve, reject) => {
+        makeHttpRequest({
+            path: `http://localhost:8080/v1/orderInfo/${eventId}`,
+            method: "get",
+        })
+            .then((res) => {
+                resolve(res || {});
+            })
+            .catch((err) => {
+                reject(err || {});
+            });
+    });
+};
+
+export const updateStatus = (orderNumber) => {
+    return new Promise((resolve, reject) => {
+        makeHttpRequest({
+            path: `http://localhost:8080/v1/orderInfo/trigger?orderNumber=${orderNumber}`,
+            method: "post",
+        })
+            .then((res) => {
+                resolve(res || {});
+            })
+            .catch((err) => {
+                reject(err || {});
+            });
+    });
+};
